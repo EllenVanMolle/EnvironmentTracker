6d7bc739f36b6e06be2b961f66ff2c374196c704
Liselot De Brabandere <debrabandereliselot@gmail.com>
2012-11-16 09:11:09 +0100

    --- version 2.6 --- database verder uitgewerkt settings eerste stappen naar timer

--- HTML5/assets/www/camera.js
+++ HTML5/assets/www/camera.js
@@ -1,14 +1,12 @@
-/**
- * color analysis
+/*
+ * Taking a picture
  */
 
-//Het linken van de knop Take a picture met de methode takeAPicture
-$('#buttonTakeAPicture').click(takeAPicture)
-
-/*Deze functie wordt aangesproken wanneer de gebruiker in stap 3 op de knop 'Take A picture' drukt. Deze functie
+/*
+ * Deze functie wordt aangesproken wanneer de gebruiker in stap 2 op de knop 'Take A picture' drukt. Deze functie
  * moet er voor zorgen dat de foto gemaakt wordt, tijdelijk opgeslagen en verwerkt wordt en nadien terug verwijderd
- * wordt.*/
-
+ * wordt.
+ */
 function takeAPicture() {
 	navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
 	    destinationType: Camera.DestinationType.DATA_URL });
@@ -18,12 +16,13 @@ function takeAPicture() {
  * Deze functie wordt aangesproken als het nemen van de foto succesvol is.
  */
 function onSuccess(imageData) {
-	var image = document.getElementById('myImage');
+	//var image = document.getElementById('myImage');
 	/*alert('image received');
 	/*image.src = "data:image/jpeg;base64," + imageData;
 	/*image.src = imageURI// ;
     localStorage.setItem("placeImage", image.src);
     alert(localStorage.getItem("placeImage"));*/
+   	pictureAnalysis(); // analyseer de foto
    }
 
 /*
@@ -31,6 +30,6 @@ function onSuccess(imageData) {
  * De gebruiker krijgt dan een foutmelding.
  */
 function onFail(message) {
-    alert('Failed because: ' + message);
+    alert('Failed because: ' + message);  
 }
  
--- HTML5/assets/www/moodSlider.js
+++ HTML5/assets/www/moodSlider.js
@@ -1,16 +1,32 @@
-
+/*
+ * Functie die wordt aangesproken op het ogenblik dat de slider van waarde verandert.
+ * De functie beschrijft hoe het gezichtje gelukkiger wordt als de mood groter wordt en
+ * verdrietiger als de mood kleiner wordt. Daarnaast wordt ook de tekst onder het gezichtje
+ * aangepast; Unhappy, Fine, Happy afh van de waarde van de mood.
+ */
 function sliderChange() {
 		
+		//variabelen
 		var mood = $("#moodSlider").val();
 		var val;
 		var newWidth;
 		var mouth = $("#mouth");
-			
-		if (mood > 5) {
+		var text = document.getElementById("moodText");
+		
+		if(mood > 6) { //als de mood groter is dan 6
+			text.innerHTML = "Happy";
+		}else if (mood < 4){ //als de mood kleiner is dan 4
+			text.innerHTML = "Unhappy";
+		}else{ //als de mood tussen 6 en 4 ligt.
+			text.innerHTML = "Fine";
+		};
+		
+		if (mood > 5) { //als de mood groter is dan 5	
 		val = 10-mood;
 		newWidth = 90 + (val*20);
        
-       	mouth.css({
+       	mouth.css({ 
+       	// Pas het css document aan zodat glimlach breder wordt als mood groter wordt.
        	top				:"auto",
        	bottom			: val*4,
        	width           : newWidth,
@@ -18,18 +34,19 @@ function sliderChange() {
         "border-radius" : newWidth / 2,
         left            : 3-(val*10)
        })
-       .removeClass("straight");
+       .removeClass("straight"); // Verwijder de standaard neutrale, rechte mond.
 					
 		}
-		else if (mood == 5 ) {
+		else if (mood == 5 ) {//als de mood gelijk is aan 5
            
-       		mouth.addClass("straight");
+       		mouth.addClass("straight"); // Zet de standaard neutrale, rechte mond.
        
      	}  
-     	else {     	
+     	else {//als de mood kleiner is dan 5  	
        	newWidth = 90 + (mood*20);
 			
 		mouth.css({
+		// Pas het css document aan zodat glimlach omgekeerd staat en breder wordt als mood kleiner wordt.
 		top				: mood*4,
 		bottom			:"auto",
 		width			: newWidth,
@@ -38,7 +55,6 @@ function sliderChange() {
         left 			: 3-(mood*10)
         })
 		.removeClass("straight");
-			
-       
+		
 		}
 	}
